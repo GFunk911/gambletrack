31a6582691b5c82551695b783bc4b536a5212861
MHarris <MHarris@PCI-MHarris.princeton.com>
2008-10-07 11:57:13 -0400

    Added show expired lines toggle

--- app/controllers/line_controller.rb
+++ app/controllers/line_controller.rb
@@ -1,6 +1,6 @@
 class LineController < ApplicationController
   def show
     @obj = get_poly_obj(params)
-    render :partial => 'game/lines', :object => @obj, :layout => false
+    render :partial => 'game/lines', :object => @obj, :layout => false, :locals => {:active => params[:active]}
   end
 end
