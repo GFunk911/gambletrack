0e9c66c2a1845f045073c42faa6892413afacaf4
MHarris <MHarris@PCI-MHarris.princeton.com>
2008-10-15 13:02:36 -0400

    Timestamp on list of lines

--- app/helpers/game_helper.rb
+++ app/helpers/game_helper.rb
@@ -2,7 +2,7 @@ module GameHelper
   def line_table_cols(lines)
     res = []
     res << 'Game' if lines.respond_to?(:games)
-    res += %w(Team Site Spread Odds Desired Outstanding Wagered Expired)
+    res += %w(Team Site Spread Odds Desired Outstanding Wagered Expired Time)
     game = lines.respond_to?(:games) ? games.first : lines
     res << 'Status' if game.played?
     res
