dc175f3d80dde069b01179e24f909e8e278463f5
MHarris <MHarris@PCI-MHarris.princeton.com>
2008-10-15 20:24:58 -0400

    Matchbook load all

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -3,7 +3,7 @@ class MainController < ApplicationController
   end
   def show
     raise "can't show" unless params[:id] == 'load_matchbook'
-    LinesDataload.new.load_matchbook!
+    LinesDataload.new.load_all!
     flash[:notice] = "Loaded lines from Matchbook"
     redirect_to :controller => 'main', :action => 'index'
   end
--- lib/lines_dataload.rb
+++ lib/lines_dataload.rb
@@ -13,4 +13,11 @@ class LinesDataload
       GameCreator.new(h).run!
     end
   end
+  def load_all!
+    load_matchbook!
+    %w(CF HK BB).each do |s|
+      load_matchbook_games!(s)
+      load_matchbook_sport!(s)
+    end
+  end
 end
