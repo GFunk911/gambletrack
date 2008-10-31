5bf0230f70317aa19af05f15ab7aa6b92411578d
Michael Harris <mharris@michael-harriss-macbook-pro.local>
2008-10-31 00:51:16 -0400

    proj total

--- app/helpers/line_helper.rb
+++ app/helpers/line_helper.rb
@@ -5,9 +5,32 @@ module LineHelper
     $bet_type_track = l.bet_type
     res
   end
-  def game_status(line)
-    d = @divs.find { |e| e.game == line.game }
+  def game_div(line)
+    @divs.find { |e| e.game == line.game }
+  end
+  def game_status_text(line)
+    d = game_div(line)
     #File.append("#{RAILS_ROOT}/debug.log",d.inspect + "\n" + @divs.size.to_s)
     d.andand.to_s_status
   end
+  def winning_team(g)
+    return g.teams[0] if g.away_score.to_i > g.home_score.to_i
+    return g.teams[1] if g.away_score.to_i < g.home_score.to_i
+    nil
+  end
+  def game_status_color(line)
+    d = game_div(line)
+    return nil unless d
+    return nil unless winning_team(d)
+    t = line.game.sport.find_team(winning_team(d))
+    return 'Green' if t.id == line.team_id
+    return 'Red'
+  end
+  def game_status2(line)
+    "<font color=\"#{game_status_color(line)}\">#{game_status_text(line)}</font>"
+  end
+  def game_status(line)
+    game_status_text(line)
+  end
+    
 end
