8cf707ff8f70f955a4ca22c220b33ddd501fc227
Michael Harris <mharris@michael-harriss-macbook-pro.local>
2009-01-27 10:41:31 -0600

    daily summary nil check

--- app/controllers/daily_summary_controller.rb
+++ app/controllers/daily_summary_controller.rb
@@ -6,6 +6,10 @@ class GameSummary
   def method_missing(sym,*args,&b)
     game.send(sym,*args,&b)
   end
+  def spread_for_home(l)
+    return nil unless l
+    line_for_home(l).spread * -1
+  end
   def line_for_home(l)
     return l if l.team_obj == home_team_obj
     res = l.clone
@@ -14,10 +18,10 @@ class GameSummary
     res
   end
   def current_spread
-    line_for_home(lines.to_a[-1]).spread * -1
+    spread_for_home(lines.to_a[-1])
   end
   def open_spread
-    line_for_home(lines.to_a[0]).spread * -1
+    spread_for_home(lines.to_a[0])
   end
   def time
     event_dt.strftime("%I:%M %p")
@@ -26,10 +30,12 @@ class GameSummary
     consensus.to_a[-1]
   end
   def anti_public_team
+    return nil unless cons
     other = [home_team_obj,away_team_obj].reject { |x| x == cons.line.team_obj }.first
     (cons.bet_percent < 0.5) ? cons.line.team_obj : other
   end
   def anti_public_percent
+    return nil unless cons
     res = (cons.bet_percent < 0.5) ? cons.bet_percent : (1.0 - cons.bet_percent)
     res.to_perc
   end
