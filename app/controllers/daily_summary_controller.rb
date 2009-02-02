ce55056f9823dfeb879a558081baf011fb0d241d
Michael Harris <mharris@pci-akaplan2.princeton.com>
2009-02-02 14:13:16 -0500

    Fixed some stuff

--- app/controllers/daily_summary_controller.rb
+++ app/controllers/daily_summary_controller.rb
@@ -6,22 +6,29 @@ class GameSummary
   def method_missing(sym,*args,&b)
     game.send(sym,*args,&b)
   end
-  def spread_for_home(l)
+  def spread_for_contrarian(l)
     return nil unless l
-    line_for_home(l).spread * -1
+    line_for_contrarian(l).spread * -1
   end
-  def line_for_home(l)
-    return l if l.team_obj == home_team_obj
+  def line_for_contrarian(l)
+    t = (anti_public_team ? anti_public_team : home_team_obj)
+    return l if l.team_obj == t
     res = l.clone
-    res.team_obj = home_team_obj
+    res.team_obj = anti_public_team
     res.spread = res.spread * -1
     res
   end
+  fattr(:pinny) do
+    Site.find_by_name('Pinnacle')
+  end
+  fattr(:pinny_lines) do
+    lines.select { |l| l.site_id == pinny.id }.sort_by { |x| x.created_at }
+  end
   def current_spread
-    spread_for_home(lines.to_a[-1])
+    spread_for_contrarian(pinny_lines.to_a[-1])
   end
   def open_spread
-    spread_for_home(lines.to_a[0])
+    spread_for_contrarian(pinny_lines.to_a[0])
   end
   def time
     event_dt.strftime("%I:%M %p")
@@ -29,10 +36,16 @@ class GameSummary
   fattr(:cons) do
     consensus.to_a[-1]
   end
-  def anti_public_team
-    return nil unless cons
-    other = [home_team_obj,away_team_obj].reject { |x| x == cons.line.team_obj }.first
-    (cons.bet_percent < 0.5) ? cons.line.team_obj : other
+  fattr(:line_sets) do
+    game.mb_spread_line_sets(anti_public_team)
+  end
+  fattr(:anti_public_team) do
+    if cons
+      other = [home_team_obj,away_team_obj].reject { |x| x == cons.line.team_obj }.first
+      (cons.bet_percent < 0.5) ? cons.line.team_obj : other
+    else
+      nil
+    end
   end
   def anti_public_percent
     return nil unless cons
@@ -52,12 +65,7 @@ class DailySummaryController < ApplicationController
     @team = Team.find(params[:team])
     @game = Game.find(params[:id])
   end
-  def market_status_all
-    #@games = games
-    puts 'abc'
-    puts 'xyz'
-  end
-  
+
   private
   def games
     Game.on_day(Time.now).sort_by { |x| x.event_dt }.map { |x| GameSummary.new(x) }
