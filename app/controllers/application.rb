07b3ba9bc8eacf625aeb2ff6869a98189a975d0b
Mike Harris <GFunk913@gmail.com>
2009-02-10 14:15:20 -0500

    iPhone stuff

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -7,6 +7,7 @@ class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
   protect_from_forgery :secret => 'b0a876313f3f9195e9bd01473bc5cd06'
   filter_parameter_logging :password, :password_confirmation
+  before_filter :adjust_format_for_iphone
   
     
   def get_poly_obj(params)
@@ -15,4 +16,25 @@ class ApplicationController < ActionController::Base
     cls = eval(params[:obj_class])
     cls.find(params[:id])
   end
+  
+  private
+
+    # Set iPhone format if request to iphone.trawlr.com
+    def adjust_format_for_iphone
+      request.format = :iphone if iphone_request?
+    end
+
+    # Force all iPhone users to login
+    def iphone_login_required
+      if iphone_request?
+        redirect_to login_path unless logged_in?
+      end
+    end
+
+    # Return true for requests to iphone.trawlr.com
+    def iphone_request?
+      #return true
+      return (request.subdomains.first == "iphone" || params[:format] == "iphone")
+    end
+  
 end
