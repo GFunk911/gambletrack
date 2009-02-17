cdcc811257da618d94705f9ec7510db8265523f2
Mike Harris <GFunk913@gmail.com>
2009-02-17 10:35:56 -0500

    Made team csv dataload read all files in directory

--- lib/dataload.rb
+++ lib/dataload.rb
@@ -7,18 +7,34 @@ class Dataload
     File.create("#{RAILS_ROOT}/public/team_names.csv",str)
   end
   
+  def team_csv_files
+    res = []
+    res << "#{RAILS_ROOT}/public/teams.csv"
+    res << "#{RAILS_ROOT}/public/cbb_teams.csv"
+    res += Dir["#{RAILS_ROOT}/public/team_load/teams/*.csv"]
+    res.select { |x| FileTest.exists?(x) }
+  end
+  
+  def team_name_csv_files
+    res = []
+    res << "#{RAILS_ROOT}/public/team_names.csv"
+    res += Dir["#{RAILS_ROOT}/public/team_load/team_names/*.csv"]
+    res.select { |x| FileTest.exists?(x) }
+  end
+  
   def load_teams!
-    lines = File.open("#{RAILS_ROOT}/public/teams.csv") { |f| f.to_a }
-    lines.each { |ln| Team.load_csv!(ln) }
+    team_csv_files.each do |filename|
+      lines = File.open(filename) { |f| f.to_a }
+      lines.each { |ln| Team.load_csv!(ln) }
+    end
     
-    lines = File.open("#{RAILS_ROOT}/public/cbb_teams.csv") { |f| f.to_a }
-    lines.each { |ln| Team.load_csv!(ln) }
-    
-    lines = File.open("#{RAILS_ROOT}/public/team_names.csv") { |f| f.to_a }
-    if lines.size == TeamName.all.size
-      puts "Already #{lines.size} team names, not running team name load"
-    else
-      lines.each { |ln| TeamName.load_csv!(ln) }
+    team_name_csv_files.each do |filename|
+      lines = File.open(filename) { |f| f.to_a }
+      if lines.size == TeamName.all.size
+        puts "Already #{lines.size} team names, not running team name load"
+      else
+        lines.each { |ln| TeamName.load_csv!(ln) }
+      end
     end
   end
 end
