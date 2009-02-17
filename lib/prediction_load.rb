d453b5d6b724703c151ec53365a2693abc101e9a
Mike Harris <GFunk913@gmail.com>
2009-02-17 10:37:34 -0500

    Enhanced predicton load to use date in page instead of assuming today

--- lib/prediction_load.rb
+++ lib/prediction_load.rb
@@ -16,12 +16,12 @@ end
 
 class PredictionRow
   include FromHash
-  attr_accessor :line, :sport
+  attr_accessor :line, :sport, :date
   fattr(:home) { line[0...21].strip }
   fattr(:away) { line[21...39].strip }
   fattr(:prediction) { line[62..68].to_f }
   def line_hash
-    res = {:home_team => home, :away_team => away, :event_dt => Time.now, :sport => sport, :site => 'Prediction'}
+    res = {:home_team => home, :away_team => away, :event_dt => date, :sport => sport, :site => 'Prediction'}
     res.merge(:team => home, :spread => prediction, :odds => '-110')
   end
   def self.all
@@ -42,6 +42,13 @@ class PredictionLoad
     sp = {'CBB' => 'bb', 'NBA' => 'nba'}[sport]
     "http://www.thepredictiontracker.com/pred#{sp}.html"
   end
+  fattr(:date) do
+    raise "can't find date" unless doc.innerText =~ /Games of (\d\d\/\d\d\/\d\d\d\d)/
+    nums = $1.split(/\//).map { |x| x.to_i }
+    dt = Time.local(nums[2],nums[0],nums[1])
+    raise "date #{dt} is bad" unless dt > 1.days.ago and dt < Time.now + 1.days
+    dt
+  end
   fattr(:starting_line_num) do
     ind = nil
     text.split("\n").each_with_index do |x,i|
@@ -49,8 +56,8 @@ class PredictionLoad
     end
     ind + 5
   end
+  fattr(:doc) { Hpricot(open(url)) }
   fattr(:text) do
-    doc = Hpricot(open(url))
     doc.search("//pre").first.innerText
   end
   fattr(:rows) do
@@ -58,7 +65,7 @@ class PredictionLoad
       puts "#{i}: #{x}"
     end
     text.split("\n")[starting_line_num..-1].map do |x|
-      PredictionRow.new(:line => x, :sport => sport)
+      PredictionRow.new(:line => x, :sport => sport, :date => date)
     end
   end
   def line_hashes
